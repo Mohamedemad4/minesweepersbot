1278fc6527da4c1f6aef49e9e98dce4110517942
mohamedemad4 <mohamed.emad4bubble@gmail.com>
2019-05-29 20:50:00 +0200

    unconfirmed pins

---
+++ ArduinoMegaLocomotion/src/RAMPS14v.h
@@ -0,0 +1,20 @@
+#define FL_STEP_PIN         54
+#define FL_DIR_PIN          55
+#define FL_ENABLE_PIN       38
+
+#define FR_STEP_PIN         60
+#define FR_DIR_PIN          61
+#define FR_ENABLE_PIN       56
+
+#define Z_C_STEP_PIN         46
+#define Z_C_DIR_PIN          48
+#define Z_C_ENABLE_PIN       62
+
+#define BL_STEP_PIN        26
+#define BL_DIR_PIN         28
+#define BL_ENABLE_PIN      24
+
+#define BR_STEP_PIN        36
+#define BR_DIR_PIN         34
+#define BR_ENABLE_PIN      30
+
