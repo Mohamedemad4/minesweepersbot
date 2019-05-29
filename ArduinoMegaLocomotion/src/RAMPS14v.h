15f08203a63ff3e706638f79ba91fb962a8d8fef
mohamedemad4 <mohamed.emad4bubble@gmail.com>
2019-05-29 20:52:55 +0200

    the one true pin mapping

--- ArduinoMegaLocomotion/src/RAMPS14v.h
+++ ArduinoMegaLocomotion/src/RAMPS14v.h
@@ -1,14 +1,15 @@
-#define FL_STEP_PIN         54
-#define FL_DIR_PIN          55
+//inspired by .platformio/packages/framework-arduinoavr/variants/rambo/pins_arduino.h 172
+#define FL_STEP_PIN         A0
+#define FL_DIR_PIN          A1
 #define FL_ENABLE_PIN       38
 
-#define FR_STEP_PIN         60
-#define FR_DIR_PIN          61
-#define FR_ENABLE_PIN       56
+#define FR_STEP_PIN         A6
+#define FR_DIR_PIN          A7
+#define FR_ENABLE_PIN       A2
 
 #define Z_C_STEP_PIN         46
 #define Z_C_DIR_PIN          48
-#define Z_C_ENABLE_PIN       62
+#define Z_C_ENABLE_PIN       A8
 
 #define BL_STEP_PIN        26
 #define BL_DIR_PIN         28
